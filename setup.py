43eccc32408973c709cc61ac6e798506a74ba4de
Ethan Jucovy <ethan.jucovy@gmail.com>
2013-06-12 14:25:21 -0400

    update metadata

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ import sys, os
 
 version = '0.1'
 
-setup(name='django-actionkit',
+setup(name='django-actionkit-client',
       version=version,
-      description="",
+      description="A Django wrapper around Actionkit's MySQL client-db, REST API and XML-RPC API",
       long_description="""\
 """,
       classifiers=[],
